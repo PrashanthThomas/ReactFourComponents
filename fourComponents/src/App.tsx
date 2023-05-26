94adea0d3e57fb468782b93ecc09306023fc425a
Prashanth Thomas <prashanth.thomas@trivium-esolutions.com>
2023-05-26 11:26:45 +0530

    Added New Dynamic Body logic with Array List

--- fourComponents/src/App.tsx
+++ fourComponents/src/App.tsx
@@ -1,18 +1,19 @@
-import './App.css'
-import CustomFooter from './Components/CustomFooter/CustomFooter'
-import CustomHeader from './Components/CustomHeader/CustomHeader'
-import DynamicBody from './Components/DynamicBody/DynamicBody'
-import MenuSideBar from './Components/MenuSideBar/MenuSideBar'
+import "./App.css";
+import CustomFooter from "./Components/CustomFooter/CustomFooter";
+import CustomHeader from "./Components/CustomHeader/CustomHeader";
+import DynamicBody from "./Components/DynamicBody/DynamicBody";
+import MenuSideBar from "./Components/MenuSideBar/MenuSideBar";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div>
-    <CustomHeader></CustomHeader>
-    <MenuSideBar></MenuSideBar>
-    <DynamicBody></DynamicBody>
-    <CustomFooter></CustomFooter>
+      <CustomHeader></CustomHeader>
+      <MenuSideBar></MenuSideBar>
+      <DynamicBody></DynamicBody>
+      <CustomFooter></CustomFooter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- fourComponents/src/Components/DynamicBody/DynamicBody.tsx
+++ fourComponents/src/Components/DynamicBody/DynamicBody.tsx
@@ -1,7 +1,25 @@
+import { MouseEvent } from "react";
+
 function DynamicBody() {
-    const para = "Test Para"
-    if (para) return <p>{para}</p>;
-    return <p>Default Para</p>
+  let cities = ["Bangalore", "Mangalore", "Delhi"];
+  const handleEvent = (event: MouseEvent) => {
+    console.log(event);
+    console.log("Text Content:" + " " + event.target.textContent);
+  };
+  // cities = [];
+  return (
+    <>
+      <h1>City list</h1>
+      {cities.length === 0 && <p>No cities to display</p>}
+      <ul className="list-group">
+        {cities.map((city, index) => (
+          <li className="list-group-item" key={index} onClick={handleEvent}>
+            {city}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default DynamicBody;
